99ee4af0f173b8ec5b5cd606b79871e52f2f59c7
ruirosiki <mr.andrew.corbin@icloud.com>
2023-05-17 11:49:44 -0600

    //Style patients/views/index

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,29 @@
-const express = require('express')
-const router = express.Router()
-const passport = require('passport')
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Express' })
-})
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Welcome" });
+});
 
 router.get(
-	'/auth/google',
-	passport.authenticate('google', { scope: ['profile', 'email'] })
-)
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
 router.get(
-	'/oauth2callback',
-	passport.authenticate('google', {
-		successRedirect: '/',
-		failureRedirect: '/',
-	})
-)
+  "/oauth2callback",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
 
-router.get('/logout', function (req, res) {
-	req.logout(function () {
-		res.redirect('/')
-	})
-})
+router.get("/logout", function (req, res) {
+  req.logout(function () {
+    res.redirect("/");
+  });
+});
 
-module.exports = router
+module.exports = router;
